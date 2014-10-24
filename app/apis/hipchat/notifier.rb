2eff61a24e54542028bd72f9f375bdceaeddcd10
Mateusz Grotha <cookie@ragnarson.com>
2014-10-24 14:33:14 +0200

    hipchat error hotfix

--- app/apis/hipchat/notifier.rb
+++ app/apis/hipchat/notifier.rb
@@ -2,15 +2,15 @@ class HipChat::Notifier
   attr_reader :client
 
   def initialize
-    @client = HipChat::Client.new(AppConfig.hipchat.api_token)
+    @client = HipChat::Client.new(AppConfig.features.hipchat.api_token)
   end
 
   def send_notification(message)
-    @client[AppConfig.hipchat.room_name].send(
+    @client[AppConfig.features.hipchat.room_name].send(
       AppConfig.hipchat.author_name,
       message,
-      notify: AppConfig.hipchat.notify_users,
-      color: AppConfig.hipchat.message_color
+      notify: AppConfig.features.hipchat.notify_users,
+      color: AppConfig.features.hipchat.message_color
     )
   end
 end
